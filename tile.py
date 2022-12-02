6e81871c1f47ddf7e2f062218d40d5b91376346d
toduko <teddyepich@gmail.com>
2022-12-02 11:23:05 +0200

    Added tile type annotations and docstrings

--- tile.py
+++ tile.py
@@ -1,44 +1,73 @@
-class Tile:
-    def __init__(self):
-        self.__mine = False
-        self.__hidden = True
-        self.__marked = False
-        self.__neighbour_mines = 0
+'''
+Tile module
+'''
 
-    def is_hidden(self):
+
+class Tile:
+    '''
+    This class represents a single tile.
+    It can be either a mine or not.
+    It can be hidden or shown.
+    Also information on neighbouring mines can be stored.
+    '''
+
+    def __init__(self):
+        self.__mine: bool = False
+        self.__hidden: bool = True
+        self.__marked: bool = False
+        self.__neighbour_mines: int = 0
+
+    def is_hidden(self) -> bool:
+        '''
+        Checks if a tile is hidden
+        '''
         return self.__hidden
 
     def show(self):
-        if not self.__hidden:
-            print("Tile already shown")
-
+        '''
+        Shows unmarked tile
+        '''
         if not self.__marked:
             self.__hidden = False
-        else:
-            print("Cannot show marked tiles")
 
-    def is_marked(self):
+    def is_marked(self) -> bool:
+        '''
+        Checks if a tile is marked
+        '''
         return self.__marked
 
     def toggle_marked(self):
+        '''
+        Toggles the tile marked state if it is hidden
+        '''
         if self.__hidden:
             self.__marked ^= True
-        else:
-            print("Cannot mark shown tiles")
 
     def set_mine(self):
+        '''
+        Sets the tile to a mine
+        '''
         self.__mine = True
 
-    def is_mine(self):
+    def is_mine(self) -> bool:
+        '''
+        Checks if the tile is a mine
+        '''
         return self.__mine
 
-    def get_neighbour_mines(self):
+    def get_neighbour_mines(self) -> int:
+        '''
+        Returns the amount of neighbouring mines
+        '''
         return self.__neighbour_mines
 
-    def set_neighbour_mines(self, amount):
+    def set_neighbour_mines(self, amount: int):
+        '''
+        Sets the amount of neighbouring mines
+        '''
         self.__neighbour_mines = amount
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.__marked:
             return 'P'
         if self.__hidden:
