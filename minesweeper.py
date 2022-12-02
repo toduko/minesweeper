742682c2eb27fe1fcd3c48eaded33e5e6bc2181e
toduko <teddyepich@gmail.com>
2022-12-02 12:51:43 +0200

    Added minesweeper type annotations and docstrings

--- minesweeper.py
+++ minesweeper.py
@@ -1,10 +1,31 @@
+'''
+Minesweeper module
+'''
+
+import sys
+
 from gui import GUI
 from cli import CLI
 
-if __name__ == '__main__':
-    use_gui = True
+if __name__ == "__main__":
+    args = sys.argv[1:]
 
-    if use_gui:
+    if len(args) > 1:
+        raise Exception("Too much arguments")
+
+    USE_GUI: bool
+
+    if not args:
+        USE_GUI = True
+    else:
+        if args[0].lower() == "gui":
+            USE_GUI = True
+        elif args[0].lower() == "cli":
+            USE_GUI = False
+        else:
+            raise Exception("Invalid arguments")
+
+    if USE_GUI:
         GUI()
     else:
         CLI()
